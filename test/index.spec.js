d12e0e82a59ba457ab649026fce861a9661e97f9
Michael <michaelzanggl@gmail.com>
2020-12-10 20:43:57 +0900

    simplify tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,73 +1,73 @@
 const test = require('japa')
 const { Cli } = require('../dist')
 
-let simulatedContext
-const returnContext = (context) => simulatedContext = context
-function fakeCli(...buildingBlocks) {
-  const cli = new Cli()
-  for (const [method, routeName] of buildingBlocks) {
-    if (method === 'route') {
-      cli[method](routeName, returnContext)
-    } else {
-      cli[method](returnContext)
-    }
-  }
-
-  return {
-    exec(args) {
-      process.argv = ['node', 'fake-cli', ...args.split(' ')]
-      cli.serve()
-      return simulatedContext
-    }
-  }
+function run(cli, args) {
+  process.argv = args.split(' ')
+  cli.serve()
 }
 
 test('will call default route if no argument is provided', async assert => {
-  const cli = fakeCli(['default'])
-  const context = cli.exec('-h')
-  assert.deepEqual(context, {"name": undefined, "args":[],"flags":{"h":true},"params":{}})
+  assert.plan(1)
+  const cli = new Cli().default(context => {
+    assert.deepEqual(context, {"name": undefined, "args":[],"flags":{"h":true},"params":{}})
+  })
+
+  run(cli, 'node my-cli -h')
 })
 
 test('will crash for unknown route when no fallback is provided', async assert => {
-  const cli = fakeCli()
+  const cli = new Cli()
   assert.plan(1)
   try {
-    cli.exec('unknown')
+    run(cli, 'node my-cli unknown')
   } catch(error) {
     assert.include(error.message, 'route "unknown" was not found.')
   }
 })
 
 test('will call fallback if route was not found', async assert => {
-  const cli = fakeCli(['fallback'])
-  const context = cli.exec('does-not-exist')
-  assert.deepEqual(context, {"name":"does-not-exist","args":[],"flags":{},"params":{}})
+  assert.plan(1)
+  const cli = new Cli().fallback(context => {
+    assert.deepEqual(context, {"name":"does-not-exist","args":[],"flags":{},"params":{}})
+  })
+
+  run(cli, 'node my-cli does-not-exist')
 })
 
 test('can call route with arguments and flags', async assert => {
-  const cli = fakeCli(['route', 'with-args'])
-  const context = cli.exec('with-args and more -i=1 -b hey')
-  assert.deepEqual(context, {"name":"with-args","args":["and","more"],"flags":{"i":1,"b":"hey"},"params":{}})
+  assert.plan(1)
+  const cli = new Cli().route('with-args', context => {
+    assert.deepEqual(context, {"name":"with-args","args":["and","more"],"flags":{"i":1,"b":"hey"},"params":{}})
+  })
+
+  run(cli, 'node my-cli with-args and more -i=1 -b hey')
 })
 
 test('can call route with params', async assert => {
-  const cli = fakeCli(['route', 'with-params {param1} {param2}'])
-  const context = cli.exec('with-params yay yayo -i=1 -b=hey')
-  assert.deepEqual(context, {"name":"with-params","args":[],"flags":{"i":1,"b":"hey"},"params":{"param1":"yay","param2":"yayo"}})
+  assert.plan(1)
+  const cli = new Cli().route('with-params {param1} {param2}', context => {
+    assert.deepEqual(context, {"name":"with-params","args":[],"flags":{"i":1,"b":"hey"},"params":{"param1":"yay","param2":"yayo"}})
+  })
+
+  run(cli, 'node my-cli with-params yay yayo -i=1 -b=hey')
 })
 
 test('can not call route with params without specifying all params', async assert => {
-  const cli = fakeCli(['route', 'with-params {param1} {param2}'])
   assert.plan(1)
+  const cli = new Cli().route('with-params {param1} {param2}', () => {})
+
   try {
-    cli.exec('with-params this-needs-two')
+    run(cli, 'node my-cli with-params this-needs-two')
   } catch(error) {
     assert.include(error.message, 'parameter param2 is missing!')
   }
 })
 
 test('can call route with params without specifying all params if they are optional', async assert => {
-  const cli = fakeCli(['route', 'with-optional {param1?}'])
-  const context = cli.exec('with-optional')
-  assert.deepEqual(context, {"name":"with-optional","args":[],"flags":{},"params":{"param1":null}})
+  assert.plan(1)
+  const cli = new Cli().route('with-optional {param1?}', context => {
+    assert.deepEqual(context, {"name":"with-optional","args":[],"flags":{},"params":{"param1":null}})
+  })
+
+  run(cli, 'node my-cli with-optional')
 })
